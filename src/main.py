3cfe0100ce2978a6039ebbb4a6ca6ed5aac6179f
Muhammad Awais <owaiskhan6605@gmail.com>
2025-08-14 13:56:10 +0500

    add enhacement feature

--- src/main.py
+++ src/main.py
@@ -1,16 +1,18 @@
 import os
 import cv2
 import time
+import torch
 import numpy as np
 from PIL import Image
 from rembg import remove
-
+from realesrgan import RealESRGANer
+from basicsr.archs.rrdbnet_arch import RRDBNet
 # Input image
 
 def main():
 
-    image_path = r"G:\Zeb-Photo-App\assets\raw_photos\input-3.jpeg"
-    output_path = r"G:\Zeb-Photo-App\output.png"
+    image_path = r"assets\raw_photos\input-3.jpeg"
+    output_path = r"output.png"
 
     # Validate file
     valid_extensions = ['.jpg', '.jpeg', '.png']
@@ -25,9 +27,11 @@ def main():
 
     resized_crop, final_w, final_h = resize_image(faces, image)
 
-    output = white_bg(resized_crop, final_w, final_h)
+    white_background = white_bg(resized_crop, final_w, final_h)
 
-    output.save(output_path)
+    enhance_image = image_enhancement(np.array(white_background))
+
+    enhance_image.save(output_path)
 
     print(f"Passport photo with clean white background saved: {output_path}")
 
@@ -95,6 +99,32 @@ def white_bg(resized_crop, final_w, final_h):
     white_bg = Image.new("RGB", (final_w, final_h), (255, 255, 255))
     white_bg.paste(no_bg, mask=no_bg.split()[3])  # Use alpha channel as mask
     return white_bg
+
+
+def image_enhancement(image):
+    # Load Real-ESRGAN model
+    model_path = "src/RealESRGAN_x4plus.pth"
+
+    state_dict = torch.load(model_path, map_location=torch.device('cpu'))['params_ema']
+
+    model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=4)
+    model.load_state_dict(state_dict, strict=True)
+
+    upsampler = RealESRGANer(
+        scale=4,
+        model_path=model_path,
+        model=model,
+        tile=0,
+        tile_pad=10,
+        pre_pad=0,
+        half=False,
+    )
+
+    image = np.array(image)
+
+    output, _ = upsampler.enhance(image, outscale=4)
+
+    return Image.fromarray(output)
 
 
 if __name__ == "__main__":
